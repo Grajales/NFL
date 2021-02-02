4edb7949a9b0eaa1e14203c72f6be263785db890
LG29878 <grajalesliliana@johndeere.com>
2021-02-01 21:26:26 -0500

    NFL query and more, answers from 1-8

--- nfl.sql
+++ nfl.sql
@@ -1,29 +1,51 @@
 -- 1. List the names of all NFL teams'
 
-
+SELECT NAME
+FROM TEAMS
+ORDER BY NAME
 -- 2. List the stadium name and head coach of all NFC teams
-
+SELECT (STADIUM,HEAD_COACH)
+FROM TEAMS
+WHERE CONFERENCE = 'NFC'
 
 -- 3. List the head coaches of the AFC South
-
+SELECT HEAD_COACH
+FROM TEAMS
+WHERE CONFERENCE = 'AFC'
+AND DIVISION = 'South'
 
 -- 4. The total number of players in the NFL
-
+SELECT COUNT(DISTINCT(ID))
+FROM PLAYERS;
 
 -- "COMMIT: NFL - queried some NFL stuff"
 
 
 -- 5. The team names and head coaches of the NFC North and AFC East
-
+(SELECT (NAME,HEAD_COACH)
+FROM TEAMS
+WHERE CONFERENCE = 'AFC'
+AND DIVISION = 'East')
+UNION
+(SELECT (NAME,HEAD_COACH)
+FROM TEAMS
+WHERE CONFERENCE = 'NFC'
+AND DIVISION = 'North')
 
 -- 6. The 50 players with the highest salaries
-
+SELECT name
+FROM PLAYERS
+ORDER BY SALARY DESC
+LIMIT 50
 
 -- 7. The average salary of all NFL players
-
+SELECT AVG(salary) AS AverageSalary FROM players;
 
 -- 8. The names and positions of players with a salary above 10_000_000
-
+SELECT (name,position)
+FROM PLAYERS
+Where SALARY>10000000
+ORDER BY SALARY DESC
 
 -- "COMMIT: NFL - wow there's a lot of nfl data"
 
